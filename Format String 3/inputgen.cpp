36a2b767fc691004d1cbeac1a8ad8feb15c153fc
Muhammad Abdullah Khan <fict.0501@gmail.com>
2026-03-15 01:57:00 +0500

    Add files via upload

---
+++ Format String 3/inputgen.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int end;
+    cout<<"Enter the end: ";
+    cin>>end;
+    cout<<"AAAA";
+    for (int i = 1; i<=end; i++){
+        cout<<"|"<<"%p";
+    }
+    return 0;
+}
